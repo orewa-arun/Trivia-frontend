417db36dc38b90a7b9e5371944ed05033cf15107
orewa-arun <arunworkacc0@gmail.com>
2025-06-08 16:01:17 +0530

    add:design redesigned landing page

--- src/features/trivia/components/LandingPage.tsx
+++ src/features/trivia/components/LandingPage.tsx
@@ -18,19 +18,19 @@ const LandingPage = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+    <div className="name-input-bg flex flex-col items-center justify-center min-h-screen px-4 text-center">
       <div className="max-w-md w-full">
-        <h1 className="text-4xl md:text-5xl font-bold text-blue-600 mb-6">
+        <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">
           THE GREAT INDIAN TRIVIA
         </h1>
-        <p className="text-lg text-gray-700 mb-8">
+        <p className="text-lg text-white mb-8">
           Test your knowledge of Indian History and win glory!
         </p>
 
         {!loading && (
           <button
             onClick={handleStart}
-            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-2xl transition-all shadow-lg"
+            className="bg-orange-600 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-2xl transition-all shadow-lg"
           >
             Start
           </button>
